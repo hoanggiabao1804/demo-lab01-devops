33ff6b6650a50fb1a694894749b2b63d98a09b09
hoanggiabao1804 <hoanggiabao1804@gmail.com>
2026-04-25 21:55:49 +0700

    Fix errors in backoffice-bff pipeline.

--- .pipelines/backoffice-bff-ci.groovy
+++ .pipelines/backoffice-bff-ci.groovy
@@ -18,163 +18,165 @@ def call(Map params) {
         )
     }
 
-    stage('Gitleak Scan') {
-        sh '''
-        echo "Run Gitleaks scan..."
-        gitleaks detect \
-        --source ./backoffice-bff \
-        --no-git \
-        --report-path gitleaks-report.json \
-        --report-format json \
-        --exit-code 0
-        '''
+    // stage('Gitleak Scan') {
+    //     sh '''
+    //     echo "Run Gitleaks scan..."
+    //     gitleaks detect \
+    //     --source ./backoffice-bff \
+    //     --no-git \
+    //     --report-path gitleaks-report.json \
+    //     --report-format json \
+    //     --exit-code 0
+    //     '''
 
-        sh '''
-        jq -r '
-        if length == 0 then
-        "<p>No secrets detected</p>"
-        else
-        "<html>
-        <head>
-        <style>
-        body { font-family: Arial; padding: 20px; }
-        h2 { margin-bottom: 20px; }
+    //     sh '''
+    //     jq -r '
+    //     if length == 0 then
+    //     "<p>No secrets detected</p>"
+    //     else
+    //     "<html>
+    //     <head>
+    //     <style>
+    //     body { font-family: Arial; padding: 20px; }
+    //     h2 { margin-bottom: 20px; }
 
-        table {
-        border-collapse: collapse;
-        width: 100%;
-        }
+    //     table {
+    //     border-collapse: collapse;
+    //     width: 100%;
+    //     }
 
-        th, td {
-        border: 1px solid #ddd;
-        padding: 10px;
-        text-align: left;
-        }
+    //     th, td {
+    //     border: 1px solid #ddd;
+    //     padding: 10px;
+    //     text-align: left;
+    //     }
 
-        th {
-        background-color: #f4f4f4;
-        }
+    //     th {
+    //     background-color: #f4f4f4;
+    //     }
 
-        tr:nth-child(even) {
-        background-color: #fafafa;
-        }
-        </style>
-        </head>
-        <body>
+    //     tr:nth-child(even) {
+    //     background-color: #fafafa;
+    //     }
+    //     </style>
+    //     </head>
+    //     <body>
 
-        <h2>Gitleaks Report</h2>
+    //     <h2>Gitleaks Report</h2>
 
-        <table>
-        <thead>
-        <tr>
-        <th>File</th>
-        <th>RuleID</th>
-        <th>Secret</th>
-        <th>StartLine</th>
-        </tr>
-        </thead>
-        <tbody>
-        " +
+    //     <table>
+    //     <thead>
+    //     <tr>
+    //     <th>File</th>
+    //     <th>RuleID</th>
+    //     <th>Secret</th>
+    //     <th>StartLine</th>
+    //     </tr>
+    //     </thead>
+    //     <tbody>
+    //     " +
 
-        (
-        [.[] |
-            "<tr>" +
-            "<td>" + .File + "</td>" +
-            "<td>" + .RuleID + "</td>" +
-            "<td>" + .Secret + "</td>" +
-            "<td>" + .StartLine + "</td>" +
-            "</tr>"
-        ] | join("")
-        )
+    //     (
+    //     [.[] |
+    //         "<tr>" +
+    //         "<td>" + .File + "</td>" +
+    //         "<td>" + .RuleID + "</td>" +
+    //         "<td>" + .Secret + "</td>" +
+    //         "<td>" + .StartLine + "</td>" +
+    //         "</tr>"
+    //     ] | join("")
+    //     )
 
-        + "
+    //     + "
 
-        </tbody>
-        </table>
+    //     </tbody>
+    //     </table>
 
-        </body>
-        </html>
-        "
-        end
-        '  gitleaks-report.json > gitleaks-report.html
-        '''
+    //     </body>
+    //     </html>
+    //     "
+    //     end
+    //     '  gitleaks-report.json > gitleaks-report.html
+    //     '''
 
-        publishHTML([
-            reportDir: '.',
-            reportFiles: 'gitleaks-report.html',
-            reportName: 'Gitleak Report',
-            allowMissing: true,
-            alwaysLinkToLastBuild: true,
-            keepAll: true
-        ])
+    //     publishHTML([
+    //         reportDir: '.',
+    //         reportFiles: 'gitleaks-report.html',
+    //         reportName: 'Gitleak Report',
+    //         allowMissing: true,
+    //         alwaysLinkToLastBuild: true,
+    //         keepAll: true
+    //     ])
 
-        def hasLeak = sh(
-            script: '[ grep -q "RuleID" gitleaks-report.json ]',
-            returnStatus: true
-        )
+    //     def hasLeak = sh(
+    //         script: '[ grep -q "RuleID" gitleaks-report.json ]',
+    //         returnStatus: true
+    //     )
 
-        if (hasLeak == 0) {
-            sh '''
-            echo "Secrets detected!"
-            '''
-        } else {
-            sh '''
-            echo "No secrets detected!"
-            '''
-        }
-    }
+    //     if (hasLeak == 0) {
+    //         sh '''
+    //         echo "Secrets detected!"
+    //         '''
+    //     } else {
+    //         sh '''
+    //         echo "No secrets detected!"
+    //         '''
+    //     }
+    // }
 
-    stage('SonarQube Analysis') {
-        withSonarQubeEnv('My SonarQube Server') {
-            sh '''
-            mvn clean verify sonar:sonar \
-            -Dsonar.host.url=http://sonarqube:9000 \
-            -f backoffice-bff
-            '''
-        }
-        timeout(time: 1, unit: 'HOURS') {
-            waitForQualityGate abortPipeline: true
-        }
-    }
+    // stage('SonarQube Analysis') {
+    //     withSonarQubeEnv('My SonarQube Server') {
+    //         sh '''
+    //         mvn clean verify sonar:sonar \
+    //         -Dsonar.host.url=http://sonarqube:9000 \
+    //         -f backoffice-bff
+    //         '''
+    //     }
+    //     timeout(time: 1, unit: 'HOURS') {
+    //         waitForQualityGate abortPipeline: true
+    //     }
+    // }
 
-    stage('OWASP Dependency Pre-build') {
-        sh '''
-        mvn -B -q clean install -DskipTests
-        '''
-    }
+    // stage('OWASP Dependency Pre-build') {
+    //     sh '''
+    //     mvn -B -q clean install -DskipTests
+    //     '''
+    // }
 
-    stage('OWASP Dependency Check') {
-        sh '''
-        mvn org.owasp:dependency-check-maven:check \
-        -pl backoffice-bff -am \
-        -DnvdApiKey=$NVD_API_KEY \
-        -Dnvd.api.endpoint=https://services.nvd.nist.gov/rest/json/cves/2.0 \
-        -Dcisa.enabled=false \
-        -Dformat=HTML \
-        -DoutputDirectory=target/dependency-check-report \
-        -DdataDirectory=/owasp \
-        -DassemblyAnalyzerEnabled=false \
-        -DnodeAnalyzerEnabled=false \
-        -DpyPackageAnalyzerEnabled=false
-        '''
-    }
+    // stage('OWASP Dependency Check') {
+    //     sh '''
+    //     mvn org.owasp:dependency-check-maven:check \
+    //     -pl backoffice-bff -am \
+    //     -DnvdApiKey=$NVD_API_KEY \
+    //     -Dnvd.api.endpoint=https://services.nvd.nist.gov/rest/json/cves/2.0 \
+    //     -Dcisa.enabled=false \
+    //     -Dformat=HTML \
+    //     -DoutputDirectory=target/dependency-check-report \
+    //     -DdataDirectory=/owasp \
+    //     -DassemblyAnalyzerEnabled=false \
+    //     -DnodeAnalyzerEnabled=false \
+    //     -DpyPackageAnalyzerEnabled=false
+    //     '''
+    // }
 
-    stage('Publish OWASP Report') {
-        publishHTML([
-            reportDir: '.',
-            reportFiles: '**/target/dependency-check-report.html',
-            reportName: 'OWASP Dependency Check Report',
-            allowMissing: true,
-            alwaysLinkToLastBuild: true,
-            keepAll: true
-        ])
-    }
+    // stage('Publish OWASP Report') {
+    //     publishHTML([
+    //         reportDir: '.',
+    //         reportFiles: '**/target/dependency-check-report.html',
+    //         reportName: 'OWASP Dependency Check Report',
+    //         allowMissing: true,
+    //         alwaysLinkToLastBuild: true,
+    //         keepAll: true
+    //     ])
+    // }
 
     stage('Snyk Scan') {
 		sh '''
+        mvn -B -q clean install -DskipTests
+
 		snyk auth $SNYK_TOKEN
 
-        snyk test --all-projects --package-manager=maven --json > snyk-report.json || true
+        snyk test --all-projects -d
 		'''
 
         sh '''
