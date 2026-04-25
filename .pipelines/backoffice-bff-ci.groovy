19ae677e6d03f6ca5ae86a82b4d7dac31138e612
hoanggiabao1804 <hoanggiabao1804@gmail.com>
2026-04-25 21:30:56 +0700

    Fix errors in backoffice-bff pipeline.

--- .pipelines/backoffice-bff-ci.groovy
+++ .pipelines/backoffice-bff-ci.groovy
@@ -174,84 +174,8 @@ def call(Map params) {
 		sh '''
 		snyk auth $SNYK_TOKEN
 
-		snyk test --file=pom.xml --package-manager=maven --json > snyk-report.json || true
-
         snyk test --file=backoffice-bff/pom.xml --package-manager=maven --json > snyk-backoffice-bff-report.json || true
 		'''
-
-        sh '''
-        echo "Publish to html..."
-
-        jq -r '
-        if (.vulnerabilities | length) == 0 then
-        "<p>No vulnerabilities</p>"
-        else
-        "<html>
-        <head>
-        <style>
-        body { font-family: Arial; padding: 20px; }
-        h2 { margin-bottom: 20px; }
-
-        table {
-        border-collapse: collapse;
-        width: 100%;
-        }
-
-        th, td {
-        border: 1px solid #ddd;
-        padding: 10px;
-        text-align: left;
-        }
-
-        th {
-        background-color: #f4f4f4;
-        }
-
-        tr:nth-child(even) {
-        background-color: #fafafa;
-        }
-        </style>
-        </head>
-        <body>
-
-        <h2>Snyk Vulnerability Report</h2>
-
-        <table>
-        <thead>
-        <tr>
-        <th>Severity</th>
-        <th>Package</th>
-        <th>Version</th>
-        <th>Title</th>
-        <th>Fixed In</th>
-        </tr>
-        </thead>
-        <tbody>
-        " +
-
-        (
-        [.vulnerabilities[] |
-            "<tr>" +
-            "<td>" + .severity + "</td>" +
-            "<td>" + .packageName + "</td>" +
-            "<td>" + .version + "</td>" +
-            "<td>" + .title + "</td>" +
-            "<td>" + (if .fixedIn then (.fixedIn | join(", ")) else "N/A" end) + "</td>" +
-            "</tr>"
-        ] | join("")
-        )
-
-        + "
-
-        </tbody>
-        </table>
-
-        </body>
-        </html>
-        "
-        end
-        ' snyk-report.json > snyk-report.html
-        '''
 
         sh '''
         jq -r '
@@ -327,7 +251,7 @@ def call(Map params) {
 
 		publishHTML([
 			reportDir: '.',
-			reportFiles: 'snyk-report.html,snyk-backoffice-bff-report.html',
+			reportFiles: 'snyk-backoffice-bff-report.html',
 			reportName: 'Snyk Report',
 			allowMissing: true,
 			alwaysLinkToLastBuild: true,
