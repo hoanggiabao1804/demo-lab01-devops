79eab3bcb994d84f2687273c47eb150d2eeb0c32
hoanggiabao1804 <hoanggiabao1804@gmail.com>
2026-04-28 14:55:26 +0700

    Add test phase in backoffice-bff pipeline. Modify JaCoCo plugin in pom.xml.

--- .pipelines/backoffice-bff-ci.groovy
+++ .pipelines/backoffice-bff-ci.groovy
@@ -170,110 +170,131 @@ def call(Map params) {
     //     ])
     // }
 
-    stage('Snyk Scan') {
-		sh '''
-		snyk auth $SNYK_TOKEN
+    // stage('Snyk Scan') {
+	// 	sh '''
+	// 	snyk auth $SNYK_TOKEN
 
-        find . -name "mvnw" -exec chmod +x {} \\;
+    //     find . -name "mvnw" -exec chmod +x {} \\;
 
-        snyk test --file=pom.xml --package-manager=maven -d --json > snyk-report.json || true
-		'''
+    //     snyk test --file=pom.xml --package-manager=maven -d --json > snyk-report.json || true
+	// 	'''
 
+    //     sh '''
+    //     jq -r '
+    //     if (.vulnerabilities | length) == 0 then
+    //     "<p>No vulnerabilities</p>"
+    //     else
+    //     "<html>
+    //     <head>
+    //     <style>
+    //     body { font-family: Arial; padding: 20px; }
+    //     h2 { margin-bottom: 20px; }
+
+    //     table {
+    //     border-collapse: collapse;
+    //     width: 100%;
+    //     }
+
+    //     th, td {
+    //     border: 1px solid #ddd;
+    //     padding: 10px;
+    //     text-align: left;
+    //     }
+
+    //     th {
+    //     background-color: #f4f4f4;
+    //     }
+
+    //     tr:nth-child(even) {
+    //     background-color: #fafafa;
+    //     }
+    //     </style>
+    //     </head>
+    //     <body>
+
+    //     <h2>Snyk Vulnerability Report</h2>
+
+    //     <table>
+    //     <thead>
+    //     <tr>
+    //     <th>Severity</th>
+    //     <th>Package</th>
+    //     <th>Version</th>
+    //     <th>Title</th>
+    //     <th>Fixed In</th>
+    //     </tr>
+    //     </thead>
+    //     <tbody>
+    //     " +
+
+    //     (
+    //     [.vulnerabilities[] |
+    //         "<tr>" +
+    //         "<td>" + .severity + "</td>" +
+    //         "<td>" + .packageName + "</td>" +
+    //         "<td>" + .version + "</td>" +
+    //         "<td>" + .title + "</td>" +
+    //         "<td>" + (if .fixedIn then (.fixedIn | join(", ")) else "N/A" end) + "</td>" +
+    //         "</tr>"
+    //     ] | join("")
+    //     )
+
+    //     + "
+
+    //     </tbody>
+    //     </table>
+
+    //     </body>
+    //     </html>
+    //     "
+    //     end
+    //     ' snyk-report.json > snyk-report.html
+	// 	'''
+
+	// 	publishHTML([
+	// 		reportDir: '.',
+	// 		reportFiles: 'snyk-report.html',
+	// 		reportName: 'Snyk Report',
+	// 		allowMissing: true,
+	// 		alwaysLinkToLastBuild: true,
+	// 		keepAll: true
+	// 	])
+
+	// 	def hasVuln = sh(
+	// 		script: 'grep -q "vulnerabilities" snyk-report.json',
+	// 		returnStatus: true
+	// 	)
+
+	// 	if (hasVuln == 0) {
+	// 		sh '''
+    //         echo "Snyk vulnerabilities found!"
+    //         '''
+	// 	} else {
+    //         sh '''
+    //         echo "No vulnerabilites found!"
+    //         '''
+    //     }
+    // }
+
+    stage('Test') {
         sh '''
-        jq -r '
-        if (.vulnerabilities | length) == 0 then
-        "<p>No vulnerabilities</p>"
-        else
-        "<html>
-        <head>
-        <style>
-        body { font-family: Arial; padding: 20px; }
-        h2 { margin-bottom: 20px; }
+        mvn clean test jacoco:report jacoco:check \
+        -f backoffice-bff
+        '''
+    }
 
-        table {
-        border-collapse: collapse;
-        width: 100%;
-        }
+    stage('Publish Test Result') {
+        junit 'backoffice-bff/**/target/surefire-reports/*.xml'
+    }
 
-        th, td {
-        border: 1px solid #ddd;
-        padding: 10px;
-        text-align: left;
-        }
-
-        th {
-        background-color: #f4f4f4;
-        }
-
-        tr:nth-child(even) {
-        background-color: #fafafa;
-        }
-        </style>
-        </head>
-        <body>
-
-        <h2>Snyk Vulnerability Report</h2>
-
-        <table>
-        <thead>
-        <tr>
-        <th>Severity</th>
-        <th>Package</th>
-        <th>Version</th>
-        <th>Title</th>
-        <th>Fixed In</th>
-        </tr>
-        </thead>
-        <tbody>
-        " +
-
-        (
-        [.vulnerabilities[] |
-            "<tr>" +
-            "<td>" + .severity + "</td>" +
-            "<td>" + .packageName + "</td>" +
-            "<td>" + .version + "</td>" +
-            "<td>" + .title + "</td>" +
-            "<td>" + (if .fixedIn then (.fixedIn | join(", ")) else "N/A" end) + "</td>" +
-            "</tr>"
-        ] | join("")
-        )
-
-        + "
-
-        </tbody>
-        </table>
-
-        </body>
-        </html>
-        "
-        end
-        ' snyk-report.json > snyk-report.html
-		'''
-
-		publishHTML([
-			reportDir: '.',
-			reportFiles: 'snyk-report.html',
-			reportName: 'Snyk Report',
-			allowMissing: true,
-			alwaysLinkToLastBuild: true,
-			keepAll: true
-		])
-
-		def hasVuln = sh(
-			script: 'grep -q "vulnerabilities" snyk-report.json',
-			returnStatus: true
-		)
-
-		if (hasVuln == 0) {
-			sh '''
-            echo "Snyk vulnerabilities found!"
-            '''
-		} else {
-            sh '''
-            echo "No vulnerabilites found!"
-            '''
-        }
+    stage('Publish Coverage Report') {
+        publishHTML([
+            reportDir: 'backoffice-bff/target/site/jacoco',
+            reportFiles: 'index.html',
+            reportName: 'JaCoCo Coverage',
+            keepAll: true,
+            alwaysLinkToLastBuild: true
+        ])
     }
 }
 
