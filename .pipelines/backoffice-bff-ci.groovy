b50397db9436c5806141081285a9eb517b28309e
hoanggiabao1804 <hoanggiabao1804@gmail.com>
2026-04-28 13:55:34 +0700

    Modify backoffice-bff pipeline.

--- .pipelines/backoffice-bff-ci.groovy
+++ .pipelines/backoffice-bff-ci.groovy
@@ -176,7 +176,7 @@ def call(Map params) {
 
         find . -name "mvnw" -exec chmod +x {} \\;
 
-        snyk test --file=pom.xml --package-manager=maven -d --json > snyk-report.json || true
+        snyk test --file=pom.xml --package-manager=maven -d --json > snyk-report.json
 		'''
 
         sh '''
