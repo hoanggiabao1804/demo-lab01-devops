15b95cb1cdbe1dd9e83fc9518267b63123b32223
hoanggiabao1804 <hoanggiabao1804@gmail.com>
2026-04-29 22:13:10 +0700

    Add sonarqube scan stage in cart pipeline.

--- .pipelines/cart-ci.groovy
+++ .pipelines/cart-ci.groovy
@@ -135,18 +135,18 @@ def call(Map params) {
         }
     }
 
-    // stage('SonarQube Analysis') {
-    //     withSonarQubeEnv('My SonarQube Server') {
-    //         sh '''
-    //         mvn clean verify sonar:sonar \
-    //         -Dsonar.host.url=http://sonarqube:9000 \
-    //         -f cart
-    //         '''
-    //     }
-    //     timeout(time: 1, unit: 'HOURS') {
-    //         waitForQualityGate abortPipeline: true
-    //     }
-    // }
+    stage('SonarQube Analysis') {
+        withSonarQubeEnv('My SonarQube Server') {
+            sh '''
+            mvn clean verify sonar:sonar \
+            -Dsonar.host.url=http://sonarqube:9000 \
+            -f cart
+            '''
+        }
+        timeout(time: 1, unit: 'HOURS') {
+            waitForQualityGate abortPipeline: true
+        }
+    }
 
     // stage('OWASP Dependency Pre-build') {
     //     sh '''
