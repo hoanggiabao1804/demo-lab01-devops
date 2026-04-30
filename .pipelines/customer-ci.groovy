4b0953c2d34496235978d71afb208e5ce4bc7fd3
hoanggiabao1804 <hoanggiabao1804@gmail.com>
2026-04-30 10:14:51 +0700

    Add Gitleaks scan stage in customer pipeline.

--- .pipelines/customer-ci.groovy
+++ .pipelines/customer-ci.groovy
@@ -52,111 +52,111 @@ def call(Map params) {
         ])
     }
 
-    // stage('Gitleak Scan') {
-    //     sh '''
-    //     echo "Run Gitleaks scan..."
-    //     gitleaks detect \
-    //     --source ./customer \
-    //     --no-git \
-    //     --report-path customer-gitleaks-report.json \
-    //     --report-format json \
-    //     --exit-code 0
-    //     '''
+    stage('Gitleak Scan') {
+        sh '''
+        echo "Run Gitleaks scan..."
+        gitleaks detect \
+        --source ./customer \
+        --no-git \
+        --report-path customer-gitleaks-report.json \
+        --report-format json \
+        --exit-code 0
+        '''
 
-    //     sh '''
-    //     jq -r '
-    //     if length == 0 then
-    //     "<p>No secrets detected</p>"
-    //     else
-    //     "<html>
-    //     <head>
-    //     <style>
-    //     body { font-family: Arial; padding: 20px; }
-    //     h2 { margin-bottom: 20px; }
+        sh '''
+        jq -r '
+        if length == 0 then
+        "<p>No secrets detected</p>"
+        else
+        "<html>
+        <head>
+        <style>
+        body { font-family: Arial; padding: 20px; }
+        h2 { margin-bottom: 20px; }
 
-    //     table {
-    //     border-collapse: collapse;
-    //     width: 100%;
-    //     }
+        table {
+        border-collapse: collapse;
+        width: 100%;
+        }
 
-    //     th, td {
-    //     border: 1px solid #ddd;
-    //     padding: 10px;
-    //     text-align: left;
-    //     }
+        th, td {
+        border: 1px solid #ddd;
+        padding: 10px;
+        text-align: left;
+        }
 
-    //     th {
-    //     background-color: #f4f4f4;
-    //     }
+        th {
+        background-color: #f4f4f4;
+        }
 
-    //     tr:nth-child(even) {
-    //     background-color: #fafafa;
-    //     }
-    //     </style>
-    //     </head>
-    //     <body>
+        tr:nth-child(even) {
+        background-color: #fafafa;
+        }
+        </style>
+        </head>
+        <body>
 
-    //     <h2>Gitleaks Report</h2>
+        <h2>Gitleaks Report</h2>
 
-    //     <table>
-    //     <thead>
-    //     <tr>
-    //     <th>File</th>
-    //     <th>RuleID</th>
-    //     <th>Secret</th>
-    //     <th>StartLine</th>
-    //     </tr>
-    //     </thead>
-    //     <tbody>
-    //     " +
+        <table>
+        <thead>
+        <tr>
+        <th>File</th>
+        <th>RuleID</th>
+        <th>Secret</th>
+        <th>StartLine</th>
+        </tr>
+        </thead>
+        <tbody>
+        " +
 
-    //     (
-    //     [.[] |
-    //         "<tr>" +
-    //         "<td>" + .File + "</td>" +
-    //         "<td>" + .RuleID + "</td>" +
-    //         "<td>" + .Secret + "</td>" +
-    //         "<td>" + (.StartLine | tostring) + "</td>" +
-    //         "</tr>"
-    //     ] | join("")
-    //     )
+        (
+        [.[] |
+            "<tr>" +
+            "<td>" + .File + "</td>" +
+            "<td>" + .RuleID + "</td>" +
+            "<td>" + .Secret + "</td>" +
+            "<td>" + (.StartLine | tostring) + "</td>" +
+            "</tr>"
+        ] | join("")
+        )
 
-    //     + "
+        + "
 
-    //     </tbody>
-    //     </table>
+        </tbody>
+        </table>
 
-    //     </body>
-    //     </html>
-    //     "
-    //     end
-    //     '  customer-gitleaks-report.json > customer-gitleaks-report.html
-    //     '''
+        </body>
+        </html>
+        "
+        end
+        '  customer-gitleaks-report.json > customer-gitleaks-report.html
+        '''
 
-    //     publishHTML([
-    //         reportDir: '.',
-    //         reportFiles: 'customer-gitleaks-report.html',
-    //         reportName: 'Gitleak Report',
-    //         allowMissing: true,
-    //         alwaysLinkToLastBuild: true,
-    //         keepAll: true
-    //     ])
+        publishHTML([
+            reportDir: '.',
+            reportFiles: 'customer-gitleaks-report.html',
+            reportName: 'Gitleak Report',
+            allowMissing: true,
+            alwaysLinkToLastBuild: true,
+            keepAll: true
+        ])
 
-    //     def hasLeak = sh(
-    //         script: '[ grep -q "RuleID" customer-gitleaks-report.json ]',
-    //         returnStatus: true
-    //     )
+        def hasLeak = sh(
+            script: '[ grep -q "RuleID" customer-gitleaks-report.json ]',
+            returnStatus: true
+        )
 
-    //     if (hasLeak == 0) {
-    //         sh '''
-    //         echo "Secrets detected!"
-    //         '''
-    //     } else {
-    //         sh '''
-    //         echo "No secrets detected!"
-    //         '''
-    //     }
-    // }
+        if (hasLeak == 0) {
+            sh '''
+            echo "Secrets detected!"
+            '''
+        } else {
+            sh '''
+            echo "No secrets detected!"
+            '''
+        }
+    }
 
     // stage('SonarQube Analysis') {
     //     withSonarQubeEnv('My SonarQube Server') {
