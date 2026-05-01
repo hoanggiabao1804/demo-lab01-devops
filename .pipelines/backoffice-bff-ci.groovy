a705e039614da5bd5453c795888aefa362e3acfe
hoanggiabao1804 <hoanggiabao1804@gmail.com>
2026-05-01 20:17:21 +0700

    Disable security checks in backoffice-bff pipeline.

--- .pipelines/backoffice-bff-ci.groovy
+++ .pipelines/backoffice-bff-ci.groovy
@@ -46,71 +46,71 @@ def call(Map params) {
         ])
     }
 
-    stage('Gitleak Scan') {
-        sh '''
-        echo "Run Gitleaks scan..."
-        gitleaks detect \
-        --source ./backoffice-bff \
-        --no-git \
-        --report-path backoffice-bff-gitleaks-report.json \
-        --report-format json \
-        --exit-code 0
-        '''
+    // stage('Gitleak Scan') {
+    //     sh '''
+    //     echo "Run Gitleaks scan..."
+    //     gitleaks detect \
+    //     --source ./backoffice-bff \
+    //     --no-git \
+    //     --report-path backoffice-bff-gitleaks-report.json \
+    //     --report-format json \
+    //     --exit-code 0
+    //     '''
 
-        def gitleaksUtils = load '.pipelines/utils/gitleaks-utils.groovy'
-        gitleaksUtils.jsonToHtml(
-            'backoffice-bff-gitleaks-report.json', 
-            'backoffice-bff-gitleaks-report.html'
-        )
+    //     def gitleaksUtils = load '.pipelines/utils/gitleaks-utils.groovy'
+    //     gitleaksUtils.jsonToHtml(
+    //         'backoffice-bff-gitleaks-report.json', 
+    //         'backoffice-bff-gitleaks-report.html'
+    //     )
 
-        publishHTML([
-            reportDir: '.',
-            reportFiles: 'backoffice-bff-gitleaks-report.html',
-            reportName: 'Gitleak Report',
-            allowMissing: true,
-            alwaysLinkToLastBuild: true,
-            keepAll: true
-        ])
-    }
+    //     publishHTML([
+    //         reportDir: '.',
+    //         reportFiles: 'backoffice-bff-gitleaks-report.html',
+    //         reportName: 'Gitleak Report',
+    //         allowMissing: true,
+    //         alwaysLinkToLastBuild: true,
+    //         keepAll: true
+    //     ])
+    // }
 
-    stage('SonarQube Analysis') {
-        withSonarQubeEnv('My SonarQube Server') {
-            sh '''
-            mvn clean verify sonar:sonar \
-            -Dsonar.host.url=http://sonarqube:9000 \
-            -f backoffice-bff \
-            -DskipITs=true
-            '''
-        }
-        timeout(time: 1, unit: 'HOURS') {
-            waitForQualityGate abortPipeline: true
-        }
-    }
+    // stage('SonarQube Analysis') {
+    //     withSonarQubeEnv('My SonarQube Server') {
+    //         sh '''
+    //         mvn clean verify sonar:sonar \
+    //         -Dsonar.host.url=http://sonarqube:9000 \
+    //         -f backoffice-bff \
+    //         -DskipITs=true
+    //         '''
+    //     }
+    //     timeout(time: 1, unit: 'HOURS') {
+    //         waitForQualityGate abortPipeline: true
+    //     }
+    // }
 
-    stage('Snyk Scan') {
-		sh '''
-		snyk auth $SNYK_TOKEN
+    // stage('Snyk Scan') {
+	// 	sh '''
+	// 	snyk auth $SNYK_TOKEN
 
-        find . -name "mvnw" -exec chmod +x {} \\;
+    //     find . -name "mvnw" -exec chmod +x {} \\;
 
-        snyk test --file=pom.xml --package-manager=maven -d --json > snyk-report.json || true
-		'''
+    //     snyk test --file=pom.xml --package-manager=maven -d --json > snyk-report.json || true
+	// 	'''
 
-        def snykUtils = load '.pipelines/utils/snyk-utils.groovy'
-        snykUtils.jsonToHtml(
-            'snyk-report.json', 
-            'snyk-report.html'
-        )
+    //     def snykUtils = load '.pipelines/utils/snyk-utils.groovy'
+    //     snykUtils.jsonToHtml(
+    //         'snyk-report.json', 
+    //         'snyk-report.html'
+    //     )
 
-		publishHTML([
-			reportDir: '.',
-			reportFiles: 'snyk-report.html',
-			reportName: 'Snyk Report',
-			allowMissing: true,
-			alwaysLinkToLastBuild: true,
-			keepAll: true
-		])
-    }
+	// 	publishHTML([
+	// 		reportDir: '.',
+	// 		reportFiles: 'snyk-report.html',
+	// 		reportName: 'Snyk Report',
+	// 		allowMissing: true,
+	// 		alwaysLinkToLastBuild: true,
+	// 		keepAll: true
+	// 	])
+    // }
 }
 
 return this
