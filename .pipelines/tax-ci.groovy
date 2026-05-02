6dfa8099f7376167f0c7d49f8cc7fc0417f2b9b7
hoanggiabao1804 <hoanggiabao1804@gmail.com>
2026-05-02 10:26:37 +0700

    Disable security checks in tax pipeline.

--- .pipelines/tax-ci.groovy
+++ .pipelines/tax-ci.groovy
@@ -52,72 +52,72 @@ def call(Map params) {
         ])
     }
 
-    stage('Gitleak Scan') {
-        sh '''
-        echo "Run Gitleaks scan..."
-        gitleaks detect \
-        --source ./tax \
-        --no-git \
-        --report-path tax-gitleaks-report.json \
-        --report-format json \
-        --exit-code 0
-        '''
+    // stage('Gitleak Scan') {
+    //     sh '''
+    //     echo "Run Gitleaks scan..."
+    //     gitleaks detect \
+    //     --source ./tax \
+    //     --no-git \
+    //     --report-path tax-gitleaks-report.json \
+    //     --report-format json \
+    //     --exit-code 0
+    //     '''
 
-        def gitleaksUtils = load '.pipelines/utils/gitleaks-utils.groovy'
-        gitleaksUtils.jsonToHtml(
-            'tax-gitleaks-report.json',
-            'tax-gitleaks-report.html'
-        )
+    //     def gitleaksUtils = load '.pipelines/utils/gitleaks-utils.groovy'
+    //     gitleaksUtils.jsonToHtml(
+    //         'tax-gitleaks-report.json',
+    //         'tax-gitleaks-report.html'
+    //     )
 
-        publishHTML([
-            reportDir: '.',
-            reportFiles: 'tax-gitleaks-report.html',
-            reportName: 'Gitleak Report',
-            allowMissing: true,
-            alwaysLinkToLastBuild: true,
-            keepAll: true
-        ])
-    }
+    //     publishHTML([
+    //         reportDir: '.',
+    //         reportFiles: 'tax-gitleaks-report.html',
+    //         reportName: 'Gitleak Report',
+    //         allowMissing: true,
+    //         alwaysLinkToLastBuild: true,
+    //         keepAll: true
+    //     ])
+    // }
 
-    stage('SonarQube Analysis') {
-        withSonarQubeEnv('My SonarQube Server') {
-            sh '''
-            mvn clean test jacoco:report sonar:sonar \
-            -pl tax \
-            -am \
-            -Djacoco.skip.check=true \
-            -Dsonar.host.url=http://sonarqube:9000 \
-            '''
-        }
-        timeout(time: 1, unit: 'HOURS') {
-            waitForQualityGate abortPipeline: true
-        }
-    }
+    // stage('SonarQube Analysis') {
+    //     withSonarQubeEnv('My SonarQube Server') {
+    //         sh '''
+    //         mvn clean test jacoco:report sonar:sonar \
+    //         -pl tax \
+    //         -am \
+    //         -Djacoco.skip.check=true \
+    //         -Dsonar.host.url=http://sonarqube:9000 \
+    //         '''
+    //     }
+    //     timeout(time: 1, unit: 'HOURS') {
+    //         waitForQualityGate abortPipeline: true
+    //     }
+    // }
 
-    stage('Snyk Scan') {
-        sh '''
-        snyk auth $SNYK_TOKEN
+    // stage('Snyk Scan') {
+    //     sh '''
+    //     snyk auth $SNYK_TOKEN
 
-        find . -name "mvnw" -exec chmod +x {} \\;
+    //     find . -name "mvnw" -exec chmod +x {} \\;
 
-        snyk test --file=pom.xml --package-manager=maven -d --json > tax-snyk-report.json || true
-        '''
+    //     snyk test --file=pom.xml --package-manager=maven -d --json > tax-snyk-report.json || true
+    //     '''
 
-        def snykUtils = load '.pipelines/utils/snyk-utils.groovy'
-        snykUtils.jsonToHtml(
-            'tax-snyk-report.json',
-            'tax-snyk-report.html'
-        )
+    //     def snykUtils = load '.pipelines/utils/snyk-utils.groovy'
+    //     snykUtils.jsonToHtml(
+    //         'tax-snyk-report.json',
+    //         'tax-snyk-report.html'
+    //     )
 
-        publishHTML([
-            reportDir: '.',
-            reportFiles: 'tax-snyk-report.html',
-            reportName: 'Snyk Report',
-            allowMissing: true,
-            alwaysLinkToLastBuild: true,
-            keepAll: true
-        ])
-    }
+    //     publishHTML([
+    //         reportDir: '.',
+    //         reportFiles: 'tax-snyk-report.html',
+    //         reportName: 'Snyk Report',
+    //         allowMissing: true,
+    //         alwaysLinkToLastBuild: true,
+    //         keepAll: true
+    //     ])
+    // }
 }
 
 return this
