72812443e7d14a112de17f2d4f75fed700a792cb
Trần Quốc Thịnh <tranquocthinh2221980@gmail.com>
2026-07-11 23:08:53 +0700

    Enable full pipeline

--- .pipelines/cart-ci.groovy
+++ .pipelines/cart-ci.groovy
@@ -23,98 +23,82 @@ def call(Map params) {
         '''
     }
 
-    // stage('Run Maven Checkstyle') {
-    //     sh '''
-    //     mvn checkstyle:checkstyle \
-    //     -pl cart \
-    //     -am \
-    //     -Dcheckstyle.output.file=reports/checkstyle/cart-checkstyle-result.xml
-    //     '''
-    // }
+    stage('Run Maven Checkstyle') {
+        sh '''
+        mvn checkstyle:checkstyle \
+        -pl cart \
+        -am \
+        -Dcheckstyle.output.file=reports/checkstyle/cart-checkstyle-result.xml
+        '''
+    }
 
-    // stage('Publish Checkstyle') {
-    //     recordIssues(
-    //         tools: [checkStyle(pattern: '**/cart-checkstyle-result.xml')]
-    //     )
-    // }
+    stage('Publish Checkstyle') {
+        recordIssues(
+            tools: [checkStyle(pattern: '**/cart-checkstyle-result.xml')]
+        )
+    }
 
-    // stage('Test') {
-    //     sh '''
-    //     mvn clean verify \
-    //     -pl cart \
-    //     -am \
-    //     -DskipITs=true
-    //     '''
-    // }
+    stage('Test') {
+        sh '''
+        mvn clean verify \
+        -pl cart \
+        -am \
+        -DskipITs=true
+        '''
+    }
 
-    // stage('Publish Test Result') {
-    //     junit 'cart/**/target/surefire-reports/*.xml'
-    // }
+    stage('Publish Test Result') {
+        junit 'cart/**/target/surefire-reports/*.xml'
+    }
 
-    // stage('Publish Coverage Report') {
-    //     publishHTML([
-    //         reportDir: 'cart/target/site/jacoco',
-    //         reportFiles: 'index.html',
-    //         reportName: 'Cart JaCoCo Coverage',
-    //         keepAll: true,
-    //         alwaysLinkToLastBuild: true
-    //     ])
-    // }
+    stage('Publish Coverage Report') {
+        publishHTML([
+            reportDir: 'cart/target/site/jacoco',
+            reportFiles: 'index.html',
+            reportName: 'Cart JaCoCo Coverage',
+            keepAll: true,
+            alwaysLinkToLastBuild: true
+        ])
+    }
 
-    // stage('Gitleak Scan') {
-    //     sh '''
-    //     echo "Run Gitleaks scan..."
-    //     gitleaks detect \
-    //     --source ./cart \
-    //     --no-git \
-    //     --report-path reports/gitleaks/cart-gitleaks-report.json \
-    //     --report-format json \
-    //     --exit-code 0
-    //     '''
+    stage('Gitleak Scan') {
+        sh '''
+        echo "Run Gitleaks scan..."
+        gitleaks detect \
+        --source ./cart \
+        --no-git \
+        --report-path reports/gitleaks/cart-gitleaks-report.json \
+        --report-format json \
+        --exit-code 0
+        '''
 
-    //     def gitleaksUtils = load '.pipelines/utils/gitleaks-utils.groovy'
-    //     gitleaksUtils.jsonToHtml(
-    //         'reports/gitleaks/cart-gitleaks-report.json', 
-    //         'reports/gitleaks/cart-gitleaks-report.html'
-    //     )
-    // }
+        def gitleaksUtils = load '.pipelines/utils/gitleaks-utils.groovy'
+        gitleaksUtils.jsonToHtml(
+            'reports/gitleaks/cart-gitleaks-report.json', 
+            'reports/gitleaks/cart-gitleaks-report.html'
+        )
+    }
 
-    // stage('Gitleak Scan') {
-    //     sh '''
-    //     echo "Run Gitleaks scan..."
-    //     gitleaks detect \
-    //     --source ./cart \
+    stage('Gitleak Scan') {
+        sh '''
+        echo "Run Gitleaks scan..."
+        gitleaks detect \
+        --source ./cart \
 
-    // stage('SonarQube Analysis') {
-    //     withSonarQubeEnv('My SonarQube Server') {
-    //         sh '''
-    //         mvn clean verify sonar:sonar \
-    //         -pl cart \
-    //         -am \
-    //         -Dsonar.host.url=http://sonarqube:9000 \
-    //         -DskipITs=true
-    //         '''
-    //     }
-    //     timeout(time: 1, unit: 'HOURS') {
-    //         waitForQualityGate abortPipeline: true
-    //     }
-    // }
-
-    // stage('Snyk Scan') {
-    //     sh '''
-    //     snyk auth $SNYK_TOKEN
-
-    //     find . -name "mvnw" -exec chmod +x {} \\;
-
-    //     snyk test --file=pom.xml --package-manager=maven -d --json > reports/snyk/cart-snyk-report.json || true
-    //     '''
-
-    //     def snykUtils = load '.pipelines/utils/snyk-utils.groovy'
-    //     snykUtils.jsonToHtml(
-    //         'reports/snyk/cart-snyk-report.json', 
-    //         'reports/snyk/cart-snyk-report.html'
-    //     )
-    // }
+    stage('SonarQube Analysis') {
+        withSonarQubeEnv('My SonarQube Server') {
+            sh '''
+            mvn clean verify sonar:sonar \
+            -pl cart \
+            -am \
+            -Dsonar.host.url=http://sonarqube:9000 \
+            -DskipITs=true
+            '''
+        }
+        timeout(time: 1, unit: 'HOURS') {
+            waitForQualityGate abortPipeline: true
+        }
+    }
 
     stage('Snyk Scan') {
         def jsonReport = 'reports/snyk/cart-snyk-report.json'
